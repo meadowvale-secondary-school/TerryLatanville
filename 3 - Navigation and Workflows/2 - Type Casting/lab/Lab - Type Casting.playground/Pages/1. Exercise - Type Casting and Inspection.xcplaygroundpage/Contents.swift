c81685de37956ffae0eb2a25aa3b1ecf8a094c15
Terry Latanville <terry.latanville@tulip.com>
2019-05-01 09:37:02 -0400

    Type Casting
    Completed Type Casting Lab

--- 3 - Navigation and Workflows/2 - Type Casting/lab/Lab - Type Casting.playground/Pages/1. Exercise - Type Casting and Inspection.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/2 - Type Casting/lab/Lab - Type Casting.playground/Pages/1. Exercise - Type Casting and Inspection.xcplaygroundpage/Contents.swift
@@ -3,26 +3,72 @@
  
  Create a collection of type [Any], including a few doubles, integers, strings, and booleans within the collection. Print the contents of the collection.
  */
-
+let myCollection: [Any] = [3.14159, 42, "Hello World", true]
+print(myCollection)
 
 /*:
  Loop through the collection. For each integer, print "The integer has a value of ", followed by the integer value. Repeat the steps for doubles, strings and booleans.
  */
+myCollection.forEach { element in
+    switch element {
+    case is Double:
+        print("The double has a value of: \(element)")
+    case is Int:
+        print("The integer has a value of: \(element)")
+    case is String:
+        print("The string has a value of: \(element)")
+    case is Bool:
+        print("The bool has a value of: \(element)")
 
+    default:
+        print("I have no idea what \(element) is!")
+    }
+}
 
 /*:
  Create a [String : Any] dictionary, where the values are a mixture of doubles, integers, strings, and booleans. Print the key/value pairs within the collection
  */
-
-
+let myDictionary: [String: Any] = ["double": 3.14159, "int": 42, "string": "Hello World", "bool": true]
+print(myDictionary)
 /*:
  Create a variable `total` of type `Double` set to 0. Then loop through the dictionary, and add the value of each integer and double to your variable's value. For each string value, add 1 to the total. For each boolean, add 2 to the total if the boolean is `true`, or subtract 3 if it's `false`. Print the value of `total`.
  */
+var total = 0.0
+myDictionary.forEach { _, value in
+    switch value {
+    case is Double:
+        total += value as! Double
+    case is Int:
+        total += Double(exactly: value as! Int)!
+    case is String:
+        total += 1
+    case is Bool:
+        total += value as! Bool ? 2 : -3
 
-
+    default:
+        print("I have no idea how to add \(value)")
+    }
+}
+print("The total is: \(total)")
 /*:
  Create a variable `total2` of type `Double` set to 0. Loop through the collection again, adding up all the integers and doubles. For each string that you come across during the loop, attempt to convert the string into a number, and add that value to the total. Ignore booleans. Print the total.
  */
+var total2 = 0.0
+myDictionary.forEach { _, value in
+    switch value {
+    case is Double:
+        total2 += value as! Double
+    case is Int:
+        total2 += Double(exactly: value as! Int)!
+    case is String:
+        if let stringValue = Double(value as! String) {
+            total2 += stringValue
+        }
 
+    default:
+        print("I have no idea how to add \(value)")
+    }
+}
+print("The new total is: \(total2)")
 
 //: page 1 of 2  |  [Next: App Exercise - Workout Types](@next)
--- 3 - Navigation and Workflows/2 - Type Casting/lab/Lab - Type Casting.playground/Pages/2. App Exercise - Workout Types.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/2 - Type Casting/lab/Lab - Type Casting.playground/Pages/2. App Exercise - Workout Types.xcplaygroundpage/Contents.swift
@@ -45,12 +45,26 @@ var workouts: [Workout] = [
 /*:
  Write simple functions called `describeRun(runningWorkout:)` and `describeSwim(swimmingWorkout:)` that take a `Run` object and a `Swim` object, respectively. Neither should return values. Each function should print a description of the workout, including the run's cadence or the swim's stroke. Time is represented in seconds, distance is represented in meters, and cadence is represented in steps per minute.
  */
+func describeRun(runningWorkout: Run) {
+    print("You ran \(runningWorkout.distance)m in \(runningWorkout.time)s at \(runningWorkout.cadence) steps per minute!")
+}
 
+func describeSwim(swimmingWorkout: Swim) {
+    print("You swam \(swimmingWorkout.distance)m in \(swimmingWorkout.time)s using the \(swimmingWorkout.stroke) stroke!")
+}
 
 /*:
  Now loop through each workout in `workouts` and, using type casting, call either `describeRun(runningWorkout:)` or `describeSwim(swimmingWorkout:)` on each. Observe what is printed to the console.
  */
-
+workouts.forEach { workout in
+    if let swim = workout as? Swim {
+        describeSwim(swimmingWorkout: swim)
+    } else if let run = workout as? Run {
+        describeRun(runningWorkout: run)
+    } else {
+        print("I have no idea what kind of workout that is!")
+    }
+}
 
 /*:
  
